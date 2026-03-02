e798adb7b9f6f554967ec0dfda059f9998e8c39d
VARUN KUMAR K M <49015422+varun99161@users.noreply.github.com>
2026-03-03 01:05:17 +0530

    Update server.c
    added a fix for segmentation fault

--- server.c
+++ server.c
@@ -7,6 +7,20 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 
+ssize_t recv_all(int sock, void *buf,size_t len)
+{
+    size_t total = 0;
+    while(total < len)
+    {
+        ssize_t n = recv(sock, (char *)buf + total,len - total, 0);
+        if(n <= 0)
+        {
+            return n;
+        }
+        total += n;
+    }
+    return total;
+}
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: %s <port>\n", argv[0]);
@@ -34,14 +48,20 @@ int main(int argc, char *argv[]) {
     uint8_t buffer[CHUNK_SIZE], calc_hash[32];
 
     while (1) {
-        recv(client, &header, sizeof(header), 0);
+        recv_all(client, &header, sizeof(header));
+
+        if(header.payload_size > CHUNK_SIZE)
+        {
+            printf("corrupted packet detected");
+            break;
+        }
 
         if (header.flags & FLAG_END) {
             printf("Transfer complete\n");
             break;
         }
 
-        recv(client, buffer, header.payload_size, 0);
+        recv_all(client, buffer, header.payload_size);
         sha256(buffer, header.payload_size, calc_hash);
 
         if (memcmp(calc_hash, header.hash, 32) == 0) {
